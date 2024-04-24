e831a8de28960f9c2c23d0a4ea316f0bb202b964
xhir <syh1508@163.com>
2024-04-24 09:41:38 +0800

    update

--- examples/export.ts
+++ examples/export.ts
@@ -1,3 +1,4 @@
 import Icon from './views/icon/index.vue'
+import Button from './views/button/index.vue'
 
-export { Icon }
+export { Icon, Button }
---
+++ examples/views/button/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <xh-button type="warning" size="small">启动测试</xh-button>
+  <xh-button type="success" size="small">启动测试</xh-button>
+</template>
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+import { XhButton } from 'xhir-ui'
+</script>
